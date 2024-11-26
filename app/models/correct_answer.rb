0fe1fd3cad0065c88328296fbf8c1c817fa046ca
Afzal <afzalrahman123@gmail.com>
2024-11-26 15:39:26 +0530

    Add model level validations for the above models

--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -1,3 +1,7 @@
 class CorrectAnswer < ApplicationRecord
   belongs_to :question
+
+  validates :question_id, presence: true
+  validates :correct_option, presence: true, inclusion: { in: [ 0, 1, 2, 3 ] }
+  # inclusion is to allow values from a particular array
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,8 @@
 class Quiz < ApplicationRecord
+  has_many :quiz_question_mappings, dependent: :destroy
+  # quiz_question_mappings has many questions, so we can associate quiz to questions via quiz_question_mappings
+  # we can call quiz.questions through the quiz_question_mappings association.
+  has_many :questions, through: :quiz_question_mappings, dependent: :destroy
+
+  validates :title, :description, presence: true
 end
